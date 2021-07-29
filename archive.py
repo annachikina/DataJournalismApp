5c21fe3b1b82d3692de4c3ecd7f072fb6425adec
achikina@isgneuro.com <KI38o*Kw>
2021-07-29 12:46:38 +0300

    change article filtration, change ner and kw from select to input for faster work

--- archive.py
+++ archive.py
@@ -7,44 +7,55 @@ def load_page():
     st.title('Архив статей')
     st.header('На этой странице можно провести поиск по всему архиву')
 
-    st.subheader('Введите одно или несколько имён или ключевых слов')
+    st.subheader('Введите имя и/или ключевое слово')
 
-    names = otp_request.get_dedup_list("ner")
-    ne = st.multiselect('Начните вводить имя, место и/или название организации', names)
+    ne = st.text_input('Введите имя, место и/или название организации')
 
-    keywords = otp_request.get_dedup_list("kw")
-    kw = st.multiselect('Начните вводить ключевое слово', keywords)
+    kw = st.text_input('Введите ключевое слово')
 
-    st.subheader('Задайте фильтры')
-
-    genres = otp_request.get_dedup_list("topic")
-    topics = st.multiselect("Выберите одну или несколько тем", genres)
-
-    dates = st.date_input("Введите период", value=[])
-
-    sources_list = otp_request.get_dedup_list("source")
-    sources = st.multiselect('Выберите источники', sources_list)
-
-    filtered_df = otp_request.get_filtered_data(ne, topics, dates, sources)
-
-    if len(filtered_df) == 100:
-        st.write("Я нашёл более 100 статей. Показываю последние 30 из них.")
-        filtered_df = filtered_df[-30:]
-    elif len(filtered_df) > 30:
-        st.write("Всего я нашёл %d статей. Показываю последние 30 из них." % len(filtered_df))
-        filtered_df = filtered_df[-30:]
+    archive_df = otp_request.get_filtered_archive(ne, kw)
+    if len(archive_df) == 0:
+        st.write("Не удалось найти в архиве подходящие материалы, попробуйте новый поиск")
     else:
-        st.write("Всего я нашёл %d статей." % len(filtered_df))
-    article_names = [": ".join(x) for x in list(zip(filtered_df["source"], filtered_df["title"].values))]
+        st.subheader('Задайте фильтры')
 
-    selected_article = st.selectbox("Выберите статью, чтобы прочитать её текст", article_names)
+        genres = list(set(archive_df["topic"].values))
+        topics = st.multiselect("Выберите одну или несколько тем", genres)
 
-    article = filtered_df[filtered_df["title"] == selected_article.split(": ")[1]]
+        dates = st.date_input("Введите период", value=[])
 
-    st.header(article["title"].values[0])
-    st.subheader(article["date"].values[0])
-    st.subheader('Рубрика: %s' % article["topic"].values[0])
-    st.write(article["text"].values[0])
+        sources_list = list(set(archive_df["source"].values))
+        sources = st.multiselect('Выберите источники', sources_list)
+
+        filtered_df = otp_request.get_filtered_data([],
+                                                    [],
+                                                    topics,
+                                                    dates,
+                                                    sources,
+                                                    index=archive_df["index"].values)
+
+        if len(filtered_df) == 100:
+            st.write("Я нашёл более 100 статей. Показываю последние 30 из них.")
+            filtered_df = filtered_df[-30:]
+        elif len(filtered_df) > 30:
+            st.write("Всего я нашёл %d статей. Показываю последние 30 из них." % len(filtered_df))
+            filtered_df = filtered_df[-30:]
+        else:
+            st.write("Всего я нашёл %d статей." % len(filtered_df))
+        # article_names = [": ".join(x) for x in list(zip(filtered_df["source"], filtered_df["title"].values))]
+        article_names = filtered_df["index"].values
+
+        selected_article = st.selectbox("Выберите статью, чтобы прочитать её текст", article_names)
+        [date, title] = selected_article.split(": ")
+
+        # article = filtered_df[filtered_df["title"] == selected_article.split(": ")[1]]
+        article = otp_request.get_article_by_index(selected_article)["text"].values[0]
+        article_params = filtered_df[filtered_df["index"] == selected_article]
+
+        st.header(title)
+        st.subheader(date)
+        st.subheader('Рубрика: %s' % article_params["topic"].values[0])
+        st.write(article)
 
     if st.button('Посмотреть статистику'):
         analysis.load_page()
